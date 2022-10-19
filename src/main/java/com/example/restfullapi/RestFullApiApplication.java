bd4797fa31b7309bf3a21644d22a4fcdc582add0
Lorenzo Paolo Cocchinone <pallinadiriso.zp92a@simplelogin.com>
2022-10-19 09:59:50 +0200

    Initial Commit to branch

--- src/main/java/com/example/restfullapi/RestFullApiApplication.java
+++ src/main/java/com/example/restfullapi/RestFullApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RestFullApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestFullApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RestFullApiApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/restfullapi/RestFullApiApplicationTests.java
+++ src/test/java/com/example/restfullapi/RestFullApiApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class RestFullApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
